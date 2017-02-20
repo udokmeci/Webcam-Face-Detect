ef3c993747ac890d76813c137aa8abf1a1d04d49
Ugur DOKMECI <ugurdokmeci@gmail.com>
2017-02-20 13:11:44 +0300

    sit straight

--- webcam_cv3.py
+++ webcam_cv3.py
@@ -1,17 +1,21 @@
 import cv2
+import os
 import sys
 import logging as log
 import datetime as dt
 
 from time import sleep
-
+font = cv2.FONT_HERSHEY_SIMPLEX
 cascPath = sys.argv[1]
+threshold= int(sys.argv[2]);
 faceCascade = cv2.CascadeClassifier(cascPath)
 log.basicConfig(filename='webcam.log',level=log.INFO)
 
 video_capture = cv2.VideoCapture(0)
 anterior = 0
 
+
+counter = 0;
 while True:
     if not video_capture.isOpened():
         print('Unable to load camera.')
@@ -26,13 +30,33 @@ while True:
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
+        minNeighbors=7,
+        minSize=(10, 10)
     )
 
     # Draw a rectangle around the faces
+    # 
+    
+    
+    minLevel=10000;
+    cv2.line ( frame, (0,threshold),(100,threshold),(0, 255, 0), 2)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        if y<minLevel :
+            minLevel=y
+        cv2.putText(frame,'%d - %d' % (minLevel , counter) ,(10,minLevel), font, 1,(255,255,255),2)
+    if minLevel>threshold:
+        counter = counter +1
+    else:
+        if counter>5:
+            os.system('play --no-show-progress --null --channels 1 synth 0.05 sine 400')
+        
+        counter =0
+        pass
+        
+    if len(faces) > 0 and minLevel>threshold and counter > 50:
+        os.system('play --no-show-progress --null --channels 1 synth 0.1 sine 2000')
+        counter=0
 
     if anterior != len(faces):
         anterior = len(faces)
@@ -40,6 +64,7 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
